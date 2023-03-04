19132b6e42a5c79076165463c0a51ed913365faa
EliOseiTutu <HackTJ10@gmail.com>
2023-03-04 14:18:47 -0500

    Revamped EVERYTHING FINAL REVAMP

--- main.js
+++ main.js
@@ -1,54 +1,39 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+const burger = document.querySelector("nav svg");
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
+    gsap.set("body", { overflow: "auto" });
+    gsap.set("body", { overflowX: "hidden" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: 0 },
+      { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
+    );
+    gsap.set("body", { overflow: "hidden" });
+  }
+  burger.classList.toggle("active");
+});
 
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
 
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        // Perform your AJAX/Fetch login
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
 });
