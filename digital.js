5e2e6ad6e9f7230a846d65061459ed1d868fb470
MuffinSW4R <amuffintimes@gmail.com>
2023-03-04 19:53:30 -0500

    small changes

--- digital.js
+++ digital.js
@@ -2,11 +2,12 @@ setInterval(() => {
     const date = new Date();
     const hours = Math.abs(date.getHours() - 12);
     const minutes = date.getMinutes();
+    var time = 0;
     if(minutes<10){
-        const time = `${hours}:0+${minutes}`;
+        time = `${hours}:0${minutes}`;
     }
     else{
-        const time = `${hours}:${minutes}`;
+        time = `${hours}:${minutes}`;
     }
     document.getElementById("clock").innerHTML = time;
     
