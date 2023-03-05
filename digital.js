44b8c7b52d574da891bc047a0aaf9d6be453c56d
MuffinSW4R <amuffintimes@gmail.com>
2023-03-04 19:13:47 -0500

    jiblet forgot

--- digital.js
+++ digital.js
@@ -2,6 +2,12 @@ setInterval(() => {
     const date = new Date();
     const hours = Math.abs(date.getHours() - 12);
     const minutes = date.getMinutes();
-    const time = `${hours}:${minutes}`;
+    if(minutes<10){
+        const time = `${hours}:0+${minutes}`;
+    }
+    else{
+        const time = `${hours}:${minutes}`;
+    }
     document.getElementById("clock").innerHTML = time;
+    
   }, 1000);
